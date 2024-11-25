f18aed714a1b9fdfb02bda69887c4192ec533fca
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-25 22:22:56 +0530

    Update Program.cs

--- src/CS/AzOpenAIService/Program.cs
+++ src/CS/AzOpenAIService/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AzOpenAIService.Configuration;
+using AzOpenAIService.Extensions;
+using Azure.AI.OpenAI;
+using Azure;
+using HeaderFooter.Interfaces;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using IHost host = IHostExtensions.GetHostBuilder(args);
+
+IHeader header = host.Services.GetRequiredService<IHeader>();
+IFooter footer = host.Services.GetRequiredService<IFooter>();
+AzAISvcAppConfiguration appConfig = host.Services.GetRequiredService<AzAISvcAppConfiguration>();
+
+// Initialize the Azure OpenAI client
+OpenAIClient openAIClient = new(new Uri(appConfig.AzureOpenAiEndpoint!), new AzureKeyCredential(appConfig.AzureOpenAiKey!));
