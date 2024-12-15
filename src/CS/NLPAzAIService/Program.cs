e811b61b2765e0e01a1c35935acd36b65bc25ae2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-15 18:44:31 +0530

    Changes

--- src/CS/NLPAzAIService/Program.cs
+++ src/CS/NLPAzAIService/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AzAIServicesCommon.Configuration;
+using AzAIServicesCommon.Extensions;
+using HeaderFooter.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = IHostExtensions.GetHostBuilder(args);
+
+IHeader header = host.Services.GetRequiredService<IHeader>();
+IFooter footer = host.Services.GetRequiredService<IFooter>();
+AzAISvcAppConfiguration appConfig = host.Services.GetRequiredService<AzAISvcAppConfiguration>();
