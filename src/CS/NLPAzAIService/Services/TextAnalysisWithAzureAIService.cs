7e94d5bdcbfdd2717e64d12cc81b050fe30c9c35
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-15 19:10:27 +0530

    Changes

--- src/CS/NLPAzAIService/Services/TextAnalysisWithAzureAIService.cs
+++ src/CS/NLPAzAIService/Services/TextAnalysisWithAzureAIService.cs
@@ -1,4 +1,6 @@
 ﻿using AzAIServicesCommon.Configuration;
+using Azure;
+using Azure.AI.TextAnalytics;
 
 namespace NLPAzAIService.Services;
 
@@ -7,8 +9,8 @@ internal sealed class TextAnalysisWithAzureAIService
 
     public static async Task ShowTextAnalysisWithAzureAIServiceDemo(AzAISvcAppConfiguration appConfig)
     {
-        string aiSvcEndpoint = appConfig.AiServicesEndpoint;
-        string aiSvcKey = appConfig.AiServicesKey;
+        string aiSvcEndpoint = appConfig.AiServicesEndpoint!;
+        string aiSvcKey = appConfig.AiServicesKey!;
 
         if (string.IsNullOrEmpty(appConfig.AiServicesEndpoint) || string.IsNullOrEmpty(appConfig.AiServicesKey))
         {
@@ -19,38 +21,75 @@ internal sealed class TextAnalysisWithAzureAIService
         try
         {
             // Create client using endpoint and key
-
+            AzureKeyCredential credentials = new(aiSvcKey);
+            Uri endpoint = new(aiSvcEndpoint);
+            TextAnalyticsClient aiClient = new(endpoint, credentials);
 
             // Analyze each text file in the reviews folder
-            var folderPath = Path.GetFullPath("./reviews");
-            DirectoryInfo folder = new DirectoryInfo(folderPath);
+            var folderPath = Path.GetFullPath(@"D:\STSA\learn-ai102-in-2025\src\Data\TextAnalysis\reviews");
+            DirectoryInfo folder = new(folderPath);
+
             foreach (var file in folder.GetFiles("*.txt"))
             {
                 // Read the file contents
-                Console.WriteLine("\n-------------\n" + file.Name);
+                ForegroundColor = ConsoleColor.White;
+                WriteLine("\n-------------\n" + file.Name);
                 StreamReader sr = file.OpenText();
                 var text = sr.ReadToEnd();
                 sr.Close();
-                Console.WriteLine("\n" + text);
+                WriteLine("\n" + text);
 
                 // Get language
-
+                ForegroundColor = ConsoleColor.DarkCyan;
+                DetectedLanguage detectedLanguage = aiClient.DetectLanguage(text);
+                WriteLine($"\nLanguage: {detectedLanguage.Name}");
 
                 // Get sentiment
-
+                ForegroundColor = ConsoleColor.DarkGreen;
+                DocumentSentiment sentimentAnalysis = aiClient.AnalyzeSentiment(text);
+                WriteLine($"\nSentiment: {sentimentAnalysis.Sentiment}");
 
                 // Get key phrases
-
+                ForegroundColor = ConsoleColor.DarkYellow;
+                KeyPhraseCollection phrases = aiClient.ExtractKeyPhrases(text);
+                if (phrases.Count > 0)
+                {
+                    WriteLine("\nKey Phrases:");
+                    foreach (string phrase in phrases)
+                    {
+                        WriteLine($"\t{phrase}");
+                    }
+                }
 
                 // Get entities
-
+                ForegroundColor = ConsoleColor.Magenta;
+                CategorizedEntityCollection entities = aiClient.RecognizeEntities(text);
+                if (entities.Count > 0)
+                {
+                    Console.WriteLine("\nEntities:");
+                    foreach (CategorizedEntity entity in entities)
+                    {
+                        WriteLine($"\t{entity.Text} ({entity.Category})");
+                    }
+                }
 
                 // Get linked entities
-
+                ForegroundColor = ConsoleColor.Blue;
+                LinkedEntityCollection linkedEntities = aiClient.RecognizeLinkedEntities(text);
+                if (linkedEntities.Count > 0)
+                {
+                    WriteLine("\nLinks:");
+                    foreach (LinkedEntity linkedEntity in linkedEntities)
+                    {
+                        WriteLine($"\t{linkedEntity.Name} ({linkedEntity.Url})");
+                    }
+                }
+                ResetColor();
             }
         }
         catch (Exception ex)
         {
+            ResetColor();
             WriteLine(ex.Message);
         }
     }
