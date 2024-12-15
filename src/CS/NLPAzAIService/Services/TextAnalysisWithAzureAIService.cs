89b7b3b90be807b89da84c373a239ce0615477c2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-15 18:52:18 +0530

    Changes

---
+++ src/CS/NLPAzAIService/Services/TextAnalysisWithAzureAIService.cs
@@ -0,0 +1,17 @@
+﻿using AzAIServicesCommon.Configuration;
+
+namespace NLPAzAIService.Services;
+
+internal sealed class TextAnalysisWithAzureAIService
+{
+
+    public static async Task ShowTextAnalysisWithAzureAIServiceDemo(AzAISvcAppConfiguration appConfig)
+    {
+        if (string.IsNullOrEmpty(appConfig.AzureOpenAiEndpoint) || string.IsNullOrEmpty(appConfig.AzureOpenAiKey) || string.IsNullOrEmpty(appConfig.AzureOpenAiDeploymentName))
+        {
+            WriteLine("Please check your appsettings.json file for missing or incorrect values.");
+            return;
+        }
+
+    }
+}
