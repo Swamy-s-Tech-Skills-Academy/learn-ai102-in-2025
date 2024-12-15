d2b866fa7bf1661de6f76dc72995cf5e1ed49f90
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-15 18:56:21 +0530

    Skeleton

--- src/CS/NLPAzAIService/Program.cs
+++ src/CS/NLPAzAIService/Program.cs
@@ -3,9 +3,12 @@ using AzAIServicesCommon.Extensions;
 using HeaderFooter.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLPAzAIService.Services;
 
 using IHost host = IHostExtensions.GetHostBuilder(args);
 
 IHeader header = host.Services.GetRequiredService<IHeader>();
 IFooter footer = host.Services.GetRequiredService<IFooter>();
 AzAISvcAppConfiguration appConfig = host.Services.GetRequiredService<AzAISvcAppConfiguration>();
+
+await TextAnalysisWithAzureAIService.ShowTextAnalysisWithAzureAIServiceDemo(appConfig);
--- src/CS/NLPAzAIService/Services/TextAnalysisWithAzureAIService.cs
+++ src/CS/NLPAzAIService/Services/TextAnalysisWithAzureAIService.cs
@@ -7,11 +7,51 @@ internal sealed class TextAnalysisWithAzureAIService
 
     public static async Task ShowTextAnalysisWithAzureAIServiceDemo(AzAISvcAppConfiguration appConfig)
     {
-        if (string.IsNullOrEmpty(appConfig.AzureOpenAiEndpoint) || string.IsNullOrEmpty(appConfig.AzureOpenAiKey) || string.IsNullOrEmpty(appConfig.AzureOpenAiDeploymentName))
+        string aiSvcEndpoint = appConfig.AiServicesEndpoint;
+        string aiSvcKey = appConfig.AiServicesKey;
+
+        if (string.IsNullOrEmpty(appConfig.AiServicesEndpoint) || string.IsNullOrEmpty(appConfig.AiServicesKey))
         {
             WriteLine("Please check your appsettings.json file for missing or incorrect values.");
             return;
         }
 
+        try
+        {
+            // Create client using endpoint and key
+
+
+            // Analyze each text file in the reviews folder
+            var folderPath = Path.GetFullPath("./reviews");
+            DirectoryInfo folder = new DirectoryInfo(folderPath);
+            foreach (var file in folder.GetFiles("*.txt"))
+            {
+                // Read the file contents
+                Console.WriteLine("\n-------------\n" + file.Name);
+                StreamReader sr = file.OpenText();
+                var text = sr.ReadToEnd();
+                sr.Close();
+                Console.WriteLine("\n" + text);
+
+                // Get language
+
+
+                // Get sentiment
+
+
+                // Get key phrases
+
+
+                // Get entities
+
+
+                // Get linked entities
+
+            }
+        }
+        catch (Exception ex)
+        {
+            WriteLine(ex.Message);
+        }
     }
 }
