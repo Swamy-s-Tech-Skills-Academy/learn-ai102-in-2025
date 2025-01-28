ab9749d7963066f1b23883b5a2ff38922294a7eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-28 14:03:51 +0530

    Code works

--- src/CS/NLPAzAIService/Program.cs
+++ src/CS/NLPAzAIService/Program.cs
@@ -5,6 +5,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NLPAzAIService.Services;
+using Azure;
+using Azure.AI.Language.Conversations;
+using Azure.Core.Serialization;
+using Azure.Core;
+using System.Text.Json;
 
 using IHost host = IHostExtensions.GetHostBuilder(args);
 
@@ -16,26 +21,126 @@ AzAISvcAppConfiguration appConfig = host.Services.GetRequiredService<AzAISvcAppC
 try
 {
     // Get config settings from AppSettings
-    IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-    IConfigurationRoot configuration = builder.Build();
-    //Guid lsAppId = Guid.Parse(configuration["LSAppID"]);
-    string predictionEndpoint = configuration["AIServicesEndpoint"];
-    string predictionKey = configuration["AIServicesKey"];
+    if (string.IsNullOrEmpty(appConfig.AiServicesEndpoint) || string.IsNullOrEmpty(appConfig.AiServicesKey))
+    {
+        WriteLine("Please check your appsettings.json file for missing or incorrect values.");
+        return;
+    }
+
+    string predictionEndpoint = appConfig.AILanguageServiceEndpoint!;
+    string predictionKey = appConfig.AILanguageServiceKey!;
 
     // Create a client for the Language service model
+    Uri endpoint = new(predictionEndpoint);
+    AzureKeyCredential credential = new(predictionKey);
+
+    ConversationAnalysisClient client = new(endpoint, credential);
 
     // Get user input (until they enter "quit")
     string userText = "";
-    while (userText.ToLower() != "quit")
+    while (userText!.ToLower() != "quit")
     {
         WriteLine("\nEnter some text ('quit' to stop)");
-        userText = ReadLine();
-        if (userText.ToLower() != "quit")
+        userText = ReadLine()!;
+        if (userText?.ToLower() != "quit")
         {
 
             // Call the Language service model to get intent and entities
+            var projectName = "Clock";
+            var deploymentName = "production";
+
+            var data = new
+            {
+                analysisInput = new
+                {
+                    conversationItem = new
+                    {
+                        text = userText,
+                        id = "1",
+                        participantId = "1",
+                    }
+                },
+                parameters = new
+                {
+                    projectName,
+                    deploymentName,
+                    // Use Utf16CodeUnit for strings in .NET.
+                    stringIndexType = "Utf16CodeUnit",
+                },
+                kind = "Conversation",
+            };
+
+            // Send request
+            Response response = await client.AnalyzeConversationAsync(RequestContent.Create(data));
+            
+            dynamic conversationalTaskResult = response.Content.ToDynamicFromJson(JsonPropertyNames.CamelCase);
+            
+            dynamic conversationPrediction = conversationalTaskResult.Result.Prediction;
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            
+            WriteLine(JsonSerializer.Serialize(conversationalTaskResult, options));
+            WriteLine("--------------------\n");
+            WriteLine(userText);
+            var topIntent = "";
+            if (conversationPrediction.Intents[0].ConfidenceScore > 0.5)
+            {
+                topIntent = conversationPrediction.TopIntent;
+            }
 
             // Apply the appropriate action
+            switch (topIntent)
+            {
+                case "GetTime":
+                    var location = "local";
+                    // Check for a location entity
+                    foreach (dynamic entity in conversationPrediction.Entities)
+                    {
+                        if (entity.Category == "Location")
+                        {
+                            //Console.WriteLine($"Location Confidence: {entity.ConfidenceScore}");
+                            location = entity.Text;
+                        }
+                    }
+                    // Get the time for the specified location
+                    string timeResponse = GetTime(location);
+                    WriteLine(timeResponse);
+                    break;
+                case "GetDay":
+                    var date = DateTime.Today.ToShortDateString();
+                    // Check for a Date entity
+                    foreach (dynamic entity in conversationPrediction.Entities)
+                    {
+                        if (entity.Category == "Date")
+                        {
+                            //Console.WriteLine($"Location Confidence: {entity.ConfidenceScore}");
+                            date = entity.Text;
+                        }
+                    }
+                    // Get the day for the specified date
+                    string dayResponse = GetDay(date);
+                    WriteLine(dayResponse);
+                    break;
+                case "GetDate":
+                    var day = DateTime.Today.DayOfWeek.ToString();
+                    // Check for entities            
+                    // Check for a Weekday entity
+                    foreach (dynamic entity in conversationPrediction.Entities)
+                    {
+                        if (entity.Category == "Weekday")
+                        {
+                            //Console.WriteLine($"Location Confidence: {entity.ConfidenceScore}");
+                            day = entity.Text;
+                        }
+                    }
+                    // Get the date for the specified day
+                    string dateResponse = GetDate(day);
+                    WriteLine(dateResponse);
+                    break;
+                default:
+                    // Some other intent (for example, "None") was predicted
+                    WriteLine("Try asking me for the time, the day, or the date.");
+                    break;
+            }
 
         }
 
